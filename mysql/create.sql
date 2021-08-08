9620e3ce2ee4594d418bf448bab46866cf555819
jiaqing23 <jiaqing2303@gmail.com>
2021-08-08 17:28:56 +0800

    Fix SQL script

--- mysql/create.sql
+++ mysql/create.sql
@@ -1,7 +1,12 @@
+DROP DATABASE entry_task;
+
+USE entry_task;
+
+CREATE DATABASE entry_task DEFAULT CHARSET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
 CREATE TABLE user (
     username VARCHAR(100) PRIMARY KEY NOT NULL,
     nickname VARCHAR(100) NOT NULL,
     password VARCHAR(250) NOT NULL
 ) DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;
--- mysql -uroot -p123456 entry_task < create.sql 
-CREATE DATABASE entry_task DEFAULT CHARSET = utf8mb4 DEFAULT COLLATE = utf8mb4_unicode_ci;
+
--- mysql/populate.sh
+++ mysql/populate.sh
@@ -3,3 +3,4 @@ python3 datagen.py
 mysql -uroot -p123456 < create.sql 
 mysql -uroot -p123456 entry_task < insert_test.sql
 mysql -uroot -p123456 entry_task < insert.sql
+rm insert.sql
